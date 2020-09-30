874af7bba1df368be55967ec92b256d3596c6aa8
lazynessmind <lazynessmind@gmail.com>
2020-09-30 22:00:25 +0100

    Clean up and change the way that the proxy was being initialized.

--- src/main/java/com/bedrocklegends/blt/BLTweaks.java
+++ src/main/java/com/bedrocklegends/blt/BLTweaks.java
@@ -1,40 +1,25 @@
 package com.bedrocklegends.blt;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import com.bedrocklegends.blt.proxy.ClientProxy;
+import com.bedrocklegends.blt.proxy.IProxy;
+import com.bedrocklegends.blt.proxy.ServerProxy;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.bedrocklegends.blt.proxy.*;
-import java.util.stream.Collectors;
+@Mod(BLTweaks.MOD_ID)
+public class BLTweaks {
 
-
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(value="bltweaks")
-public class BLTweaks
-{
-    // Directly reference a log4j logger.
     public static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "bltweaks";
-	public static IProxy proxy =  DistExecutor.runForDist(() -> ClientProxy::new, () -> ServerProxy::new);
-	
+    public static IProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
+
     public BLTweaks() {
 
         proxy.init();
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-  
 }
