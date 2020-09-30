f656e91b9c56b66d8e70b7de2cb2cb756ac3d365
lazynessmind <lazynessmind@gmail.com>
2020-09-30 21:55:37 +0100

    Uncomment this if you plan to use this... (Mappings conflicting)

--- src/main/java/com/bedrocklegends/blt/mixins/ProgressLoadGuiMixin.java
+++ src/main/java/com/bedrocklegends/blt/mixins/ProgressLoadGuiMixin.java
@@ -1,50 +1,41 @@
 package com.bedrocklegends.blt.mixins;
 
-import net.minecraft.client.gui.LoadingGui;
 import net.minecraft.client.gui.ResourceLoadProgressGui;
-import net.minecraft.util.ColorHelper;
-
-import java.util.concurrent.ThreadLocalRandom;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 
 @Mixin(value = ResourceLoadProgressGui.class, priority = 1001)
 public abstract class ProgressLoadGuiMixin {
 
-	
-	@ModifyArg(method = {
-	"func_238629_a_" }, remap = false, at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
-private int barBorderColor(int color) {
-return ColorHelper.PackedColor.func_233006_a_(255, 185, 55, 35);
-}
+    /*@ModifyArg(method = {
+            "func_238629_a_"}, remap = false, at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
+    private int barBorderColor(int color) {
+        return ColorHelper.PackedColor.func_233006_a_(255, 185, 55, 35);
+    }
 
-@ModifyArg(method = {
-	"func_238629_a_" }, remap = false, at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
-private int barBorderColor1(int color) {
-return ColorHelper.PackedColor.func_233006_a_(255, 185, 55, 35);
-}
+    @ModifyArg(method = {
+            "func_238629_a_"}, remap = false, at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
+    private int barBorderColor1(int color) {
+        return ColorHelper.PackedColor.func_233006_a_(255, 185, 55, 35);
+    }
 
-@ModifyArg(method = {
-	"func_238629_a_" }, remap = false, at = @At(value = "INVOKE", ordinal = 2, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
-private int barBorderColor2(int color) {
-return ColorHelper.PackedColor.func_233006_a_(255, 185, 55, 35);
-}
+    @ModifyArg(method = {
+            "func_238629_a_"}, remap = false, at = @At(value = "INVOKE", ordinal = 2, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
+    private int barBorderColor2(int color) {
+        return ColorHelper.PackedColor.func_233006_a_(255, 185, 55, 35);
+    }
 
-@ModifyArg(method = {
-	"func_238629_a_" }, remap = false, at = @At(value = "INVOKE", ordinal = 3, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
-private int barBorderColor3(int color) {
-return ColorHelper.PackedColor.func_233006_a_(255, 185, 55, 35);
-}
+    @ModifyArg(method = {
+            "func_238629_a_"}, remap = false, at = @At(value = "INVOKE", ordinal = 3, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
+    private int barBorderColor3(int color) {
+        return ColorHelper.PackedColor.func_233006_a_(255, 185, 55, 35);
+    }
 
-@ModifyArg(method = {
-	"func_238629_a_" }, remap = false, at = @At(value = "INVOKE", ordinal = 4, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
-private int barColor(int color) {
-return ColorHelper.PackedColor.func_233006_a_(255, 218, 165, 32);
-}
+    @ModifyArg(method = {
+            "func_238629_a_"}, remap = false, at = @At(value = "INVOKE", ordinal = 4, target = "Lnet/minecraft/client/gui/ResourceLoadProgressGui;func_238467_a_(Lcom/mojang/blaze3d/matrix/MatrixStack;IIIII)V"), index = 5)
+    private int barColor(int color) {
+        return ColorHelper.PackedColor.func_233006_a_(255, 218, 165, 32);
+    }*/
 
-    
-	
+
 }
